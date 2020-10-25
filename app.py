0ef52c6f914035278222426a4104879446c2eed0
BENAODA TLEMCANI <212744998@ge.com>
2020-10-25 18:02:44 +0100

    dynamic routes, response objects (response, status headers), debug mode

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 # ------ Flask Hello World ------ #
 from flask import Flask 
 
+ref_name = "amine"
+
 app = Flask(__name__)
+app.config["DEBUG"]=True
+
 
 @app.route("/")
 def main_page():
@@ -9,7 +13,37 @@ def main_page():
 
 @app.route("/hello")
 def hello_world():
-	return "Hello World!" 
+	return "Hello World?!?!?!?!?!?!?!" 
+
+@app.route("/test/<search_query>")
+def search(search_query):
+	return search_query
+
+@app.route("/integer/<int:value>")
+def int_type(value):
+	print(value+1)
+	print(type(value))
+	return "correct integer"
+
+@app.route("/float/<float:value>")
+def float_type(value):
+	print(value+1)
+	print(type(value))
+	return "correct float"
+
+@app.route("/path/<path:value>")
+def path_type(value):
+	print(value)
+	print(type(value))
+	return "correct path" 
+
+@app.route("/name/<name>")
+def right_name(name):
+	if name.lower() == ref_name:
+		return "Hello, {}".format(name), 200
+	else: 
+		return "Wrong name", 400
+
 
 
 # def main_page():
